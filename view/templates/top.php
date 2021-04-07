e4626f83adffac22686acf32ddca66d421806cd8
Yeonwu <61102178+Yeonwu@users.noreply.github.com>
2021-04-07 12:26:34 +0900

    Create top.php

---
+++ view/templates/top.php
@@ -0,0 +1,25 @@
+<meta charset="UTF-8">
+
+<title>
+	별무리 매점 협동조합 
+	<?php if ( SERVER_TYPE === 'DEV' ) echo "(개발 서버)"; ?>
+</title>
+
+<meta name="google-signin-client_id" content="<?php echo GOOGLE_CLIENT_ID; ?>">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
+<link rel = "stylesheet" type = "text/css" href = "assets/css/custom.css">
+<link rel = "stylesheet" type = "text/css" href = "assets/css/w3.css">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+<!-- 로고이미지 -->
+<link rel="shortcut icon" href="./assets/img/logo.png">
+<link rel="icon" href="./assets/img/logo.png">
+
+<script src="https://kit.fontawesome.com/2ecdd499df.js" crossorigin="anonymous"></script>
